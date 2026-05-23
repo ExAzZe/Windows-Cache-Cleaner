23e30c38ea8b0a2696683407f507a995232ff80b
ExAzE <131683195+ExAzZe@users.noreply.github.com>
2026-05-23 11:57:49 +0200

    Bug fixe

--- Start.bat
+++ Start.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 net session >nul 2>&1
 if %errorLevel% == 0 goto :run
 
-powershell -Command "Start-Process '%~f0' -ArgumentList '%*' -Verb RunAs"
+powershell -Command "Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0WindowsCacheCleaner.ps1"" %*' -Verb RunAs"
 exit
 
 :run
